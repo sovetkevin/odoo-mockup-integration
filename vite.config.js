347fffb54dc65a567939cee2141cc68604add119
Kevin Sovet <kevin.sovet@gmail.com>
2026-04-09 23:17:48 +0200

    Fix: Add base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+  base: '/odoo-mockup-integration/', 
+  
   resolve: {
     alias: {
       '~': path.resolve(__dirname, 'node_modules')
